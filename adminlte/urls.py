419d31a1dc3f3533b90b2db0bbd4941ece3d05e7
Javier Lasso <125415549+Jlroja@users.noreply.github.com>
2023-11-12 09:32:15 -0500

    Dummy commit

--- adminlte/urls.py
+++ adminlte/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [ path('', views.index, name='index'), 
                 path('index3.html', views.index3, name='index3'),
+                path('login.html', views.login, name='login')
                ]
--- adminlte/views.py
+++ adminlte/views.py
@@ -5,3 +5,6 @@ def index(request):
     return render(request, 'adminlte/index.html')
 def index3(request):
     return render(request, 'adminlte/index3.html')
+def login(request):
+    return render(request, 'adminlte/login_Example/login.html')
+    

4198e7bc5a3f6836b50665c14a7f7cc762ffef90
Juan Molina <juan.sebastian.molina@correounivalle.edu.co>
2023-11-15 12:01:39 -0500

    se configura settings.py para que apunte a la base de datos postgresql

--- adminlte_project/settings.py
+++ adminlte_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'adminlte_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dsf1_proyecto',
+        'USER': 'postgres',
+        'PASSWORD': '123facil123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

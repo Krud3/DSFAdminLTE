90e2bf040acb440bef2c205ed66a9890d37ba45c
Juan Molina <juan.sebastian.molina@correounivalle.edu.co>
2023-11-15 12:02:42 -0500

    se configura urls y views para los logins y el dashboard del gerente

--- adminlte/urls.py
+++ adminlte/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [ path('login/', views.login, name='login'),
                path('index/', views.index, name='index'), 
+               path('logout/', views.logout_view, name='logout'),
                ]
--- adminlte/views.py
+++ adminlte/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth import logout
+from .models import Gerente
 
 # Create your views here.
 def index(request):
-    return render(request, 'adminlte/index.html')
+    return render(request, 'adminlte/gerente/index.html')
 def login(request):
     if request.method == 'POST':
-        #logica de autenticacion va aqui
-        #username = request.POST.get('username')
-        #password = request.POST.get('password')
-        return redirect(reverse('index'))
+        username = request.POST.get("username")
+        
+        password = request.POST.get('password')
+        try:
+            gerente = Gerente.objects.get(id_gerente=username)
+            if check_password(password, gerente.pass_field):
+                #request.session['user'] = gerente.id_gerente
+                return redirect(reverse('index'))
+            else:
+                return render(request, 'adminlte/gestion_login/login.html', {'error': 'Contraseña incorrecta'})
+            
+        except Gerente.DoesNotExist:
+            return render(request, 'adminlte/gestion_login/login.html', {'error': 'Usuario no existe'})
+        
     return render(request, 'adminlte/gestion_login/login.html')
-
+def logout_view(request):
+    logout(request)
+    return redirect('login')
